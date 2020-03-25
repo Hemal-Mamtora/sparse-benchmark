a75597affb32be191eda9a2d24b7f3f570c7fd1f
Hemal-Mamtora <hemalmamtora1@gmail.com>
2020-03-25 12:42:40 +0530

    timing code

--- src/new.cpp
+++ src/new.cpp
@@ -1,59 +1,74 @@
 // Includes all relevant components of mlpack.
 #include <mlpack/core.hpp>
+
 // Convenience.
 using namespace mlpack;
+using namespace std;
+
 int main()
 {
   // First, load the data.
-  time_t start, end;
   double time_taken; 
   arma::umat data, d, a, b;
   arma::sp_mat sp_data, c;
-  // Use data::Load() which transposes the matrix.
-  time(&start); 
+  // Use data::Load() which tramsposes the matrix.
+  auto start = chrono::steady_clock::now(); 
   data::Load("data/citeseer.edges.csv", data, true);
-  time(&end);
-  time_taken = double(end - start);
-  std::cout<<"Time taken to load a dense matrix of edge list: "
-           <<time_taken<<" sec.\n mat.shape: "<<size(data)<<std::endl;
+  auto end = chrono::steady_clock::now(); 
+  auto diff = end - start;
+  cout<<"\nTime taken to load a demse matrix of edge list: "
+           <<chrono::duration <double, milli> (diff).count() << 
+           " ms\n mat.shape: "<<size(data)<<endl;
 
-  // std::cout<<data.row(2)<<std::endl;
+  // cout<<data.row(2)<<endl;
 
-  arma::umat locations = arma::umat(data.rows( 0, 1));
+  arma::umat locatioms = arma::umat(data.rows( 0, 1));
   arma::Col<arma::u64> values = arma::Col<arma::u64>(data.row(2).t());
 
-  time(&start); 
-  arma::SpMat<arma::u64> temp_sp_data = arma::SpMat<arma::u64>(locations, values);
-  time(&end);
+  start = chrono::steady_clock::now();  
+  arma::SpMat<arma::u64> temp_sp_data = arma::SpMat<arma::u64>(locatioms, values);
+  end = chrono::steady_clock::now(); 
+  diff = end - start;
+  
+  cout<<"\nTime taken to create an adjacency matrix from edge list: "
+           <<chrono::duration <double, milli> (diff).count() << 
+           " ms\n mat.shape: "<<endl;
 
-  time_taken = double(end - start);
-  std::cout<<"Time taken to create an adjacency matrix from edge list: "
-           <<time_taken<<" sec.\n mat.shape: "<<std::endl;
+   
+  auto start1 = chrono::steady_clock::now();
+  int j;
+  for(int i = 0; i < 100000; i++){
+    //for(int k = 0; k < 100000; k++)
+      j += 1;
+  }
+  auto end1 = chrono::steady_clock::now();
 
+  diff = end1 - start1;
 
-  //17 SpMat<T1, T2>(const arma::Base<arma::uword, T1> &locations, const arma::Base<double, T2> &values, const bool sort_locations)
+  cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
+  //17 SpMat<T1, T2>(comst arma::Base<arma::uword, T1> &locatioms, comst arma::Base<double, T2> &values, comst bool sort_locatioms)
   //sp_data = arma::sp_mat(arma::mat(data.rows( 0, 1));
   //arma::umat(data.rows( 0, 1)), arma::colvec(arma::mat(data.row(2).t()))
 
-  // arma::umat locations = data.rows( 0, 1);
+  // arma::umat locatioms = data.rows( 0, 1);
   // arma::rowvec values = data.row(2);
-  // sp_data = arma::sp_mat(locations, values);
+  // sp_data = arma::sp_mat(locatioms, values);
 
-  // std::cout<<data.head_rows(1)<<std::endl;
-  // std::cout<<size(data)<<std::endl;
+  // cout<<data.head_rows(1)<<endl;
+  // cout<<size(data)<<endl;
   // Now compute the covariance.  We assume that the data is already centered.
   // Remember, because the matrix is column-major, the covariance operation is
-  // transposed.
-  // std::cout<<(data * sp_data)<<std::endl;
-  // std::cout<<(sp_data * data)<<std::endl;
-  // std::cout<<(sp_data * sp_data)<<std::endl;
-  // std::cout<<(data * data)<<std::endl;
+  // tramsposed.
+  // cout<<(data * sp_data)<<endl;
+  // cout<<(sp_data * data)<<endl;
+  // cout<<(sp_data * sp_data)<<endl;
+  // cout<<(data * data)<<endl;
   // for(int i = 0; i < 10; i++){
-  //   sp_data = sp_data * trans(data); 
-  //   std::cout<<sp_data<<std::endl;
+  //   sp_data = sp_data * trams(data); 
+  //   cout<<sp_data<<endl;
   // }
-  // // std::cout<<"Dense"<<std::endl;
-  // std::cout<<cov_dense<<std::endl;
+  // // cout<<"Demse"<<endl;
+  // cout<<cov_demse<<endl;
   
   // cov.save("sparse_covariance");
   // cov.save("sparse_covariance.txt", arma::raw_ascii);
